31d19d615ffb0beaa069f7b6fc7d8331caed3a80
Snownee <mjy523@126.com>
2019-06-16 16:20:32 +0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,106 @@
+Vue.component('card-selector', {
+  template: `
+  <div>
+    <ul>
+      <li v-for="n in 13" @click="add(Number($event.target.innerText))">{{ n }}</li>
+    </ul>
+    <ul class="my">
+      <li v-for="n in cards" @click="remove(Number($event.target.innerText))">{{ n }}</li>
+    </ul>
+  </div>
+  `,
+  methods: {
+    add: function(n) {
+      this.cards.push(n)
+      this.cards.sort(function(a, b) {return a - b})
+      this.$root.update()
+    },
+    remove: function(n) {
+      var i = this.cards.indexOf(n);
+      if (i >= 0)
+      {
+        this.cards.splice(i, 1);
+        this.$root.update()
+      }
+    }
+  },
+  data() {
+    return {
+      cards: []
+    }
+  }
+})
+
+new Vue({
+  el: '#app',
+  data: {
+    results: []
+  },
+  methods: {
+    update: function() {
+      this.results = [];
+      var fang = this.$refs.fang.cards
+      var card = new Set(this.$refs.card.cards)
+
+      if (fang.length == 0 || card.length == 0)
+      {
+        return
+      }
+
+      for (var c of card) {
+        var bits = []
+        var maxBit = 0
+        for (var i = 0; i < fang.length; i++) {
+          bits[i] = false;
+        }
+
+        var countTrue = 0;
+        do {
+
+          for (var i = 0; i < bits.length; i++) {
+            if (bits[i])
+            {
+              bits[i] = false
+            }
+            else
+            {
+              bits[i] = true
+              if (i > maxBit)
+              {
+                maxBit++
+              }
+              break
+            }
+          }
+
+          countTrue = 0
+          for (var i = 0; i <= maxBit; i++) {
+            if (bits[i])
+            {
+              countTrue++
+            }
+          }
+
+          if (countTrue > 1) // 13 + 13 < 36
+          {
+            var result = [ c ]
+            var sum = c
+            for (var i = 0; i <= maxBit; i++) {
+              if (bits[i])
+              {
+                result.push(fang[i])
+                sum += fang[i]
+              }
+            }
+            if (sum == 36)
+            {
+              this.results.push(result)
+            }
+          }
+
+        } while (countTrue < fang.length);
+      }
+      console.log(this.results);
+    }
+  }
+})
